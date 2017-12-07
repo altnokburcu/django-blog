5c35fc8e7d88c90bc43bf1c60b530769737bf56d
Burcu Altınok <burcualtnok@gmail.com>
2017-12-07 11:24:50 +0000

    blog created

---
+++ blog/models.py
@@ -0,0 +1,20 @@
+from django.db import models
+from django.utils import timezone
+
+
+class Post(models.Model): #Modelimizi nesne türünden tanımladık.
+    yazar = models.ForeignKey('auth.User')
+    baslik = models.CharField(max_length=200) #kısa metinleri tanımlar.
+    yazi = models.TextField() #uzun metinleri tanımlar.
+    #bu da gün ve saati tanımlamada kullanılır.
+    yaratilma_tarihi = models.DateTimeField(
+           default=timezone.now)
+    yayinlanma_tarihi = models.DateTimeField(
+           blank=True, null=True)
+
+    def yayinla(self):
+        self.yayinlanma_tarihi = timezone.now()
+        self.save()
+
+    def __str__(self):
+        return self.baslik
