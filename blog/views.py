116c34fa588cc215b616baf5ce1dfac4761b2f53
Burcu Altınok <burcualtnok@gmail.com>
2017-12-07 15:00:42 +0000

    query sets used, template html,css added

--- blog/views.py
+++ blog/views.py
@@ -1,4 +1,7 @@
 from django.shortcuts import render
+from django.utils import timezone
+from .models import Post
 
 def post_list(request):
-    return render(request, 'blog/post_list.html', {})
+    posts = Post.objects.filter(yayinlanma_tarihi__lte=timezone.now()).order_by('yayinlanma_tarihi')
+    return render(request, 'blog/post_list.html', {'posts': posts})
