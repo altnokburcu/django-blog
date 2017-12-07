2b026497add97f38c907b171a87a48b29bdbedfd
Burcu Altınok <burcualtnok@gmail.com>
2017-12-07 13:34:53 +0000

    html template eklendi

--- mysite/urls.py
+++ mysite/urls.py
@@ -15,7 +15,8 @@ Including another URLconf
 """
 from django.conf.urls import url
 from django.contrib import admin
-
+from django.conf.urls import include
 urlpatterns = [
-    url(r'^admin/', admin.site.urls),
+    url(r'^admin/', include(admin.site.urls)),
+    url(r'', include('blog.urls')),
 ]
